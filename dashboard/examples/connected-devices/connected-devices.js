336159d773c6cb2f34b7614cc6bb5c6cdf6a3f87
Bogdan-Ioan BRUDIU <bogdan@brudiu.ro>
2015-06-27 15:55:29 +0300

    added count & removed junk

--- dashboard/examples/connected-devices/connected-devices.js
+++ dashboard/examples/connected-devices/connected-devices.js
@@ -1,194 +1,40 @@
 var client = new Keen({
-  projectId: "5337e28273f4bb4499000000",
-  readKey: "8827959317a6a01257bbadf16c12eff4bc61a170863ca1dadf9b3718f56bece1ced94552c6f6fcda073de70bf860c622ed5937fcca82d57cff93b432803faed4108d2bca310ca9922d5ef6ea9381267a5bd6fd35895caec69a7e414349257ef43a29ebb764677040d4a80853e11b8a3f"
+  projectId: "558e66e046f9a77cf19f3fc3",
+  readKey: "87953cc9f5c74b01e068c3c9a42b602ab5532f1b26d691e4ec07be2329a0563d44da0158dc7aca12143190bc79cf76a7c4b66fe9bf4de5027bb69b142afdf51cc04976f70437a79c6b442d54135dcfb648980477f228984645b8d9a62b3ba5c8d6fc0a15f2a11cb1edd5646914299068"
 });
 
-var geoProject = new Keen({
-  projectId: "53eab6e12481962467000000",
-  readKey: "d1b97982ce67ad4b411af30e53dd75be6cf610213c35f3bd3dd2ef62eaeac14632164890413e2cc2df2e489da88e87430af43628b0c9e0b2870d0a70580d5f5fe8d9ba2a6d56f9448a3b6f62a5e6cdd1be435c227253fbe3fab27beb0d14f91b710d9a6e657ecf47775281abc17ec455"
-});
+
+
+
+
 
 Keen.ready(function(){
 
-  // ----------------------------------------
-  // Visitors Timeline
-  // ----------------------------------------
-  var new_users = new Keen.Query("count", {
-    eventCollection: "activations",
-    interval: "monthly",
-    timeframe: "this_year"
+
+
+ var query = new Keen.Query("count", {
+    eventCollection: "step",
+    timezone: "UTC"
   });
-  geoProject.draw(new_users, document.getElementById("visitors"), {
-    chartType: "areachart",
-    title: "Monthly Visits",
-    height: 300,
-    width: 490,
-    chartOptions: {
-      legend: { position: "none" },
-      chartArea: {
-        height: "78%",
-        top: "15%",
-        left: "8%",
-        width: "85%"
-      },
-      hAxis: { format: 'MMM', maxTextLines: 1 }
-    }
-  });
-
-  // ----------------------------------------
-  // Visitors by Browser Timeline
-  // ----------------------------------------
-  var browser = new Keen.Query("count", {
-    eventCollection: "activations",
-    timeframe: "this_year",
-    interval: "monthly",
-    groupBy: "device_model_name"
-  });
-  geoProject.draw(browser, document.getElementById("browser"), {
-    title: "Visits by Browser",
-    height: 300,
-    width: 475,
-    chartOptions: {
-      legend: { position: "none" },
-      chartArea: {
-        height: "78%",
-        top: "15%",
-        left: "10%",
-        width: "100%"
-      }
-    }
-  });
-
-  // ----------------------------------------
-  // Visitors by State
-  // ----------------------------------------
-  var state = new Keen.Query("count", {
-    eventCollection: "visit",
-    groupBy: "visitor.geo.province"
-  });
-  client.draw(state, document.getElementById("geography"), {
-    chartType: "columnchart",
-    title: "Visits by State",
-    height: 300,
-    width: 475,
-    chartOptions: {
-      legend: { position: "none" },
-      chartArea: {
-        height: "78%",
-        top: "15%",
-        left: "12%",
-        width: "100%"
-      }
-    },
-    labelMapping: {
-      "New Jersey" : "NJ",
-      "Virginia" : "VA",
-      "California": "CA",
-      "Washington": "WA",
-      "Utah": "UT",
-      "Oregon": "OR",
-      "null": "Other"
-    }
-  });
+  client.draw(query, document.getElementById("stepcount"), {
+  chartType: "metric",
+  title: "Step count",
+  colors: ["#49c5b1"]
+});
 
 
-  // ----------------------------------------
-  // Users
-  // ----------------------------------------
-
-  var users = new Keen.Query("count_unique", {
-    eventCollection: "activations",
-    targetProperty: "user.id"
-  });
-
-  $(".users").knob({
-    'angleArc':250,
-    'angleOffset':-125,
-    'readOnly':true,
-    'min':0,
-    'max':500,
-    'fgColor': Keen.Dataviz.defaults.colors[0],
-    height: 290,
-    width: '95%'
-  });
-  var geoUsers = geoProject.run(users, function(err, res){
-    $(".users").val(res.result).trigger('change');
-  });
 
 
-  // ----------------------------------------
-  // Errors Detected
-  // ----------------------------------------
 
 
-  var errors = new Keen.Query("count", {
-    eventCollection: "user_action",
-    filters: [{"property_name":"error_detected","operator":"eq","property_value":true}]
-  });
-
-  $(".errors").knob({
-    'angleArc':250,
-    'angleOffset':-125,
-    'readOnly':true,
-    'min':0,
-    'max':100,
-    'fgColor': Keen.Dataviz.defaults.colors[1],
-    height: 290,
-    width: '95%'
-  });
-  geoProject.run(errors, function(err, res){
-    $(".errors").val(res.result).trigger('change');
-  });
 
 
-  // ----------------------------------------
-  // Funnel
-  // ----------------------------------------
-  var funnel = new Keen.Query("funnel", {
-    steps: [
-      {
-         event_collection: "purchases",
-         actor_property: "user.id"
-      },
-      {
-        event_collection: "activations",
-        actor_property: "user.id"
-      },
-      {
-        event_collection: "status_update",
-        actor_property: "user.id"
-      },
-      {
-        event_collection: "user_action",
-        actor_property: "user.id",
-        filters: [] // where property "total_sessions" == 2
-      },
-      {
-        event_collection: "user_action",
-        actor_property: "user.id",
-        filters: [] // where property "action" equals "invited friend"
-      }
-    ]
-  });
 
-  /*  This funnel is built from mock data */
-  var sampleFunnel = { result: [ 3250, 3000, 2432, 1504, 321 ], steps: funnel.params.steps };
 
-  new Keen.Dataviz()
-    .el(document.getElementById("chart-05"))
-    .parseRawData(sampleFunnel)
-    .chartType('barchart')
-    .chartOptions({
-      chartArea: { height: "85%", left: "20%", top: "5%" },
-      legend: { position: "none" }
-    })
-    .colors([Keen.Dataviz.defaults.colors[5]])
-    .height(340)
-    .labels(["Purchased Device", "Activated Device", "First Session", "Second Session", "Invited Friend"])
-    .title(null)
-    .render();
 
-  // ----------------------------------------
+
+
+ // ----------------------------------------
   // Mapbox - Active Users
   // ----------------------------------------
   var tframe = "previous_7_days";
@@ -239,7 +85,7 @@ Keen.ready(function(){
       timeframe: tframe,
       filters: geoFilter
     });
-    var result = geoProject.run(scoped_events, function(err, res){
+    var result = client.run(scoped_events, function(err, res){
       console.log("events", res);
       activeMapData.clearLayers();
 
@@ -330,4 +176,10 @@ Keen.ready(function(){
 
 
 initialize();
+
+
+
+
+
+
 });
